033b25993538ca59710eeda61bbd1a32c643f6dd
unknown <leonardoelricon@gmail.com>
2025-07-21 00:17:29 -0300

    PM - CM08 - Bugfix02 - Erro de csfr token corrigido criando o arquivo api.php e sinalizando este no bootstra/app.php

--- app/Models/Moto.php
+++ app/Models/Moto.php
@@ -7,15 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Moto extends Model
 {
     protected $fillable = ['marca', 'modelo', 'ano', 'preco'];
-    public function up()
-    {
-        Schema::create('motos', function (Blueprint $table) {
-            $table->id();
-            $table->string('marca');
-            $table->string('modelo');
-            $table->year('ano');
-            $table->decimal('preco', 10, 2);
-            $table->timestamps();
-        });
-    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 
-use App\Http\Controllers\Api\MotoController;
+use App\Http\Controllers\Api\MotoControllerAPI;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('motos', MotoController::class);
+Route::apiResource('motos', MotoControllerAPI::class);
